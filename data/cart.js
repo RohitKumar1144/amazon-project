8b1d369337447c11941db6290e590560d354ff78
Rohit Kumar <rohit.kumar114406>
2026-06-21 14:55:35 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,5 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'))  || 
+[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
   },
@@ -7,6 +8,10 @@ export let cart = [{
   quantity: 1
   }
 ];
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
   let matchingItem;
@@ -23,6 +28,7 @@ export function addToCart(productId){
       quantity: 1
       });
     } 
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -35,4 +41,5 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;
+  saveToStorage();
 }
