464bfcadb86f0442941f76ab498c1cb7f6e06542
Rohit Kumar <rohit.kumar114406@gmail.com>
2026-06-26 16:10:46 +0530

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderCartSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderCheckoutHeader();
 renderCartSummary();
