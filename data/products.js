ba3717d828e30a525f85fe9a5d48bad92f940aa7
Rohit Kumar <rohit.kumar114406@gmail.com>
2026-06-26 17:40:09 +0530

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+ import {formatCurrency} from '../scripts/utils/money.js';
+ 
  export function getProduct(productId){
   let matchingProduct;
   products.forEach((product) =>{
@@ -6,6 +8,29 @@
     }
   });
   return matchingProduct;
+ }
+
+ class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
  }
  
  
@@ -668,4 +693,6 @@
       "mens"
     ]
   }
-];
+].map((productDetails) =>{
+  return new Product(productDetails);
+});
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderCartSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import '../data/cart-class.js';
+// import '../data/cart-class.js';
 
 renderCheckoutHeader();
 renderCartSummary();
