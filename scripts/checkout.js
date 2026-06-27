793cd923fcf3bf2e8a8fa7ee7fcdaa38dc26125f
Rohit Kumar <rohit.kumar114406@gmail.com>
2026-06-27 16:01:21 +0530

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,7 @@
 import { renderCartSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-// import '../data/cart-class.js';
-// import '../data/car.js';
+import '../data/backend-practice.js'
 
 renderCheckoutHeader();
 renderCartSummary();
